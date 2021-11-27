55b265867fffc5f8310617284077d3627455a657
Johan794 <johan.ricardo1104@gmail.com>
2021-11-27 08:59:06 -0500

    update to spring proyect

---
+++ src/main/java/ui/BackMioroutesApplication.java
@@ -0,0 +1,13 @@
+package ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackMioroutesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackMioroutesApplication.class, args);
+    }
+
+}
