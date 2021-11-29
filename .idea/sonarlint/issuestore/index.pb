45714a0f8e62a285941cee20de928399e5463c0c
CamiloCJ09 <camilocampazj@gmail.com>
2021-11-29 16:25:13 -0500

    Adding stations from de data base

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+4
+mvnw,5\d\5d837c4ed85ecaaf932c506e80ff5d7b9f3d590d
+b
+2src/main/java/service/RoutesManagementService.java,e\7\e7b2715e9c9f879851fcac790c6a35af865f6f60
+R
+"src/test/java/model/GraphTest.java,4\2\42be0a5b54ab71f75e4a94a85ec68ecb1d954c03
+_
+/src/main/java/firebase/FirebaseInitializer.java,1\7\172152b872bce7014f944a1ced83c12dab387570
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -13,15 +13,44 @@ for(let i = 0; i < 5; i++){
     opt1.text = elements[i].toString();
     select.add(opt1);
 }
+/*
 for(let i = 0; i < 5; i++){
     let opt2 = document.createElement("option");
     opt2.value = elements[i].toString();
     opt2.text = elements[i].toString();
     select2.add(opt2);
 }
+*/
 
 button.onclick = function(){
     //Add code to generate route
     console.log("Hola");
     bestRoute.innerHTML = "La mejor ruta";
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+    getData();
+});
+async function getData(){
+    const request = await fetch('vertex/get',{
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+
+    });
+
+    const routes = await request.json();
+
+    //let i = 1
+    for (let route of routes) {
+        let opt3 = document.createElement("option");
+        opt3.value = route.value;
+        opt3.text = route.name;
+        select2.add(opt3);
+    }
+
+    //document.querySelector("#tablePlayers tbody").outerHTML = listHtml
+}
+
